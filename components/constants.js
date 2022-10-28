53ec0aa59291b7913154e2ff5534062b0d8974ca
saride satishkumar <satishkumarsaride192@gmail.com>
2022-10-28 11:40:40 +0530

    background&mint schedule done

--- components/constants.js
+++ components/constants.js
@@ -7,9 +7,12 @@ module.exports = Object.freeze({
   mintPrice: "0.05 eth",
   mintVolume: "2025",
   mint_will_start: "Mint will start in",
+  mint_has_started: "You can mint now",
   mint_upto: "You can mint upto 3",
   mint_lable: "Enter the numebr",
   mint_button: "MINT",
   minimum_mints: 1,
   maximum_mints: 3,
+  mint_schedule: 1666937620000, // 13 digit timestamp in milliseconds
+  background_image: "/assets/background.jpeg",
 });
--- components/home.jsx
+++ components/home.jsx
@@ -3,9 +3,47 @@ import constants from "./constants";
 
 export default function HomePage() {
   const [mintCount, setMintCount] = useState(constants.minimum_mints);
+  const futureDate = new Date(constants.mint_schedule);
+  const [mintStarted, setMintStarted] = useState(false);
+  const [diff, setDiff] = useState({
+    day: 0,
+    hour: 0,
+    minute: 0.0,
+    month: 0,
+    second: 0.0,
+    year: 0,
+  });
+  const [timeStamp, setTimeStamp] = useState(futureDate);
+  const getDateDiff = (date1, date2) => {
+    const diff = new Date(date2.getTime() - date1.getTime());
+    return {
+      year: diff.getUTCFullYear() - 1970,
+      month: diff.getUTCMonth(),
+      day: diff.getUTCDate() - 1,
+      hour: diff.getUTCHours(),
+      minute: diff.getUTCMinutes(),
+      second: diff.getUTCSeconds(),
+    };
+  };
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setDiff(getDateDiff(new Date(), timeStamp));
+      if (new Date() > timeStamp) {
+        console.log("time is up");
+        setMintStarted(true);
+        clearInterval(timer);
+      }
+    }, 1000);
+    return () => clearInterval(timer);
+  }, [timeStamp]);
 
   return (
-    <div className="home-page h-[100vh] w-full bg-slate-400 flex flex-col justify-between">
+    <div
+      className="home-page h-[100vh] w-full bg-slate-400 flex flex-col justify-between"
+      style={{
+        backgroundImage: `url(${constants.background_image})`,
+      }}
+    >
       <div className="header w-full  pt-[3%] flex  justify-around items-center sm:flex-row flex-col">
         <h1 className="text-2xl behnschrift-font text-white sm:text-6xl">
           {constants.heading}
@@ -61,12 +99,12 @@ export default function HomePage() {
   function totalMintCount() {
     return (
       <div className="flex flex-col items-center elmessiri-font">
-        <p className="text-4xl sm:text-8xl">{constants.mintName}</p>
-        <p className="sm:text-xl sm:pt-[5px]">{constants.mintPrice}</p>
+        <p className="text-4xl sm:text-8xl text-black">{constants.mintName}</p>
+        <p className="sm:text-xl sm:pt-[5px] text-black">{constants.mintPrice}</p>
 
         <div className=" w-[120px] h-[30px] sm:h-[40px] drop-shadow-md sm:w-[140px] bg-white rounded-md mt-[15px]">
           <div className="flex flex-row w-full h-full items-center justify-center">
-            <p className="text-sm sm:text-xl">0 / {constants.mintVolume}</p>
+            <p className="text-sm sm:text-xl text-black">0 / {constants.mintVolume}</p>
           </div>
         </div>
       </div>
@@ -75,34 +113,56 @@ export default function HomePage() {
 
   function mintSection() {
     return (
-      <div className="flex flex-col items-center h-[40vh] sm:h-[60vh] m-auto justify-around w-[80vw] sm:m-1 sm:w-[30vw] avenyt-font p-5 bg-white rounded-2xl md:mr-[50px] relative">
-        <p className="text-2xl sm:text-4xl">{constants.mint_will_start}</p>
-        <div className="w-[80%] flex flex-row justify-between items-center">
-          <p className="p-2 sm:p-4 bg-blue-400 text-white rounded-md sm:text-2xl">
-            79
-          </p>
-          <span className="text-xl sm:text-4xl">:</span>
-          <p className="p-2 sm:p-4 bg-blue-400 text-white rounded-md sm:text-2xl">
-            79
-          </p>
-          <span className="text-xl sm:text-4xl">:</span>
-          <p className="p-2 sm:p-4 bg-blue-400 text-white rounded-md sm:text-2xl">
-            79
-          </p>
-        </div>
-        <p className="sm:text-2xl">{constants.mint_upto}</p>
+      <div className="flex flex-col items-center h-[40vh] sm:mb-[60px] sm:h-[60vh] m-auto justify-around w-[80vw] sm:m-1 sm:w-[30vw] avenyt-font p-5 bg-white rounded-2xl md:mr-[50px] relative">
+        <p className="text-2xl sm:text-4xl text-black">
+          {mintStarted ? constants.mint_has_started : constants.mint_will_start}
+        </p>
+        {!mintStarted && (
+          <div className="w-[80%] flex flex-row justify-between items-center">
+            <p className="p-2 sm:p-4 bg-blue-400 text-white rounded-md sm:text-2xl">
+              {diff.hour}
+            </p>
+            <span className="text-xl sm:text-4xl text-black">:</span>
+            <p className="p-2 sm:p-4 bg-blue-400 text-white rounded-md sm:text-2xl">
+              {diff.minute}
+            </p>
+            <span className="text-xl sm:text-4xl text-black">:</span>
+            <p className="p-2 sm:p-4 bg-blue-400 text-white rounded-md sm:text-2xl">
+              {diff.second}
+            </p>
+          </div>
+        )}
+        <p className="sm:text-2xl text-black">{constants.mint_upto}</p>
         <div className="flex flex-row items-center">
-          <p className="sm:text-2xl">{constants.mint_lable}</p>
+          <p className="sm:text-2xl text-black">{constants.mint_lable}</p>
           <span className="flex flex-col">
             <span className="flex flex-row">
-              <p className="pl-[15px] sm:text-2xl cursor-pointer">-</p>
-              <p className="pl-[15px] sm:text-2xl">3</p>
-              <p className="pl-[15px] sm:text-2xl cursor-pointer">+</p>
+              <p
+                className="pl-[15px] text-black sm:text-2xl cursor-pointer"
+                onClick={() => {
+                  if (mintCount > constants.minimum_mints)
+                    setMintCount(mintCount - 1);
+                }}
+              >
+                -
+              </p>
+              <p className="pl-[15px] text-black sm:text-2xl">{mintCount}</p>
+              <p
+                className="pl-[15px] sm:text-2xl text-black cursor-pointer"
+                onClick={() => {
+                  if (mintCount < constants.maximum_mints) {
+                    setMintCount(mintCount + 1);
+                  }
+                }}
+              >
+                +
+              </p>
             </span>
             <hr class="ml-[15px] w-[55px] h-1 bg-gray-300 rounded border-0 " />
           </span>
         </div>
         <button
+          disabled={!mintStarted}
           type="button"
           className="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg sm:text-2xl sm:px-5 sm:py-2.5 px-5 py-1.5 mr-2 mb-2  "
         >
