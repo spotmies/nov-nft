4ebb11550ea82ead879061bdc0ffd0b074d3f6cb
Ryanator001 <109184066+Ryanator001@users.noreply.github.com>
2022-11-01 00:04:38 +0530

    Update constants.js

--- components/constants.js
+++ components/constants.js
@@ -1,11 +1,11 @@
 module.exports = Object.freeze({
-  heading: "BORED MORTY LUBBA CLUB",
+  heading: "SCREEEEECH",
   twitterLink: "",
   openSeaLink: "",
   etherScanLink: "",
   mintName: "Mint",
-  mintPrice: "0.05 eth",
-  mintVolume: "2025",
+  mintPrice: "0.005 eth",
+  mintVolume: "3333",
   mint_will_start: "Mint will start in",
   mint_has_started: "You can mint now",
   mint_upto: "You can mint upto 3",
@@ -15,6 +15,6 @@ module.exports = Object.freeze({
   maximum_mints: 3,
   mint_schedule: 1666937620000, // 13 digit timestamp in milliseconds
   background_image: "/assets/background.jpeg",
-  mint_price: 0.05, // in eth
+  mint_price: 0.005, // in eth
   whiteList:[]
 });
